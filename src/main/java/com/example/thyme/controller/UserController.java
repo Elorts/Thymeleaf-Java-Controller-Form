ba3fbf12fa14de865a969536651e23ed57fd045b
deividas strole <deiwis@yahoo.com>
2024-09-05 12:55:58 -0700

    cleaned code

--- src/main/java/com/example/thyme/controller/UserController.java
+++ src/main/java/com/example/thyme/controller/UserController.java
@@ -26,6 +26,7 @@ public class UserController {
         return "userForm";
     }
 
+    
     @PostMapping("/submit-user")
     public String submitUser(@ModelAttribute User user) {
         System.out.println("Username: " + user.getUsername());
