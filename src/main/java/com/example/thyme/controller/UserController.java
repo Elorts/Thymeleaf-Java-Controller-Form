0e47b3b11675996759ee7008457ee1018a0fd851
deividas strole <deiwis@yahoo.com>
2024-09-12 12:59:02 -0700

    changed controller

--- src/main/java/com/example/thyme/controller/UserController.java
+++ src/main/java/com/example/thyme/controller/UserController.java
@@ -39,7 +39,7 @@ public class UserController {
             System.out.println("Postal Code: " + address.getPostalCode());
         }
 
-        return "result"; // Redirect to a success page or another view
+        return "result";
     }
-    
+
 }
