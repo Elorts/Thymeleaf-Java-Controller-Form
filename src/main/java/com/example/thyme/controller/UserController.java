b388c6d79dfee896ae5e2ed03e89e4bdc09da7d0
deividas strole <deiwis@yahoo.com>
2024-11-08 15:20:42 -0800

    Initial commit

---
+++ src/main/java/com/example/thyme/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.thyme.controller;
+
+import com.example.thyme.domain.User;
+import com.example.thyme.domain.Address;
+
+        //com.coderscampus.ChatApplication.domain.User;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/user-form")
+    public String showForm(Model model) {
+
+        System.out.println("********************************************");
+
+        User user = new User();
+        user.setAddress(new Address()); // Initialize Address within User
+        user.setPassword("firstP");
+
+        System.out.println("Password before html:" + user.getPassword());
+
+        model.addAttribute("user", user);
+        return "userForm";
+    }
+
+    @PostMapping("/submit-user")
+    public String submitUser(@ModelAttribute User user) {
+        System.out.println("Username: " + user.getUsername());
+        System.out.println("Password: " + user.getPassword());
+        System.out.println("Email: " + user.getEmail());
+
+        Address address = user.getAddress();
+        if (address != null) {
+            System.out.println("Street: " + address.getStreet());
+            System.out.println("City: " + address.getCity());
+            System.out.println("Postal Code: " + address.getPostalCode());
+        }
+
+        return "result"; // Redirect to a success page or another view
+    }
+}
---
+++ src/main/java/com/example/thyme/domain/Address.java
@@ -0,0 +1,33 @@
+package com.example.thyme.domain;
+
+
+public class Address {
+    private String street;
+    private String city;
+    private String postalCode;
+
+    // Getters and setters
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
